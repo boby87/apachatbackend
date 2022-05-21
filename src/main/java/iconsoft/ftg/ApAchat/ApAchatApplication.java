52972ac5e26325b3223aa47d59d7aec3fccad726
boby87 <fokoutghislain@gmail.com>
2022-05-21 05:16:07 +0100

    first commit

---
+++ src/main/java/iconsoft/ftg/ApAchat/ApAchatApplication.java
@@ -0,0 +1,13 @@
+package iconsoft.ftg.ApAchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApAchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApAchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iconsoft/ftg/ApAchat/ApAchatApplicationTests.java
@@ -0,0 +1,13 @@
+package iconsoft.ftg.ApAchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApAchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
